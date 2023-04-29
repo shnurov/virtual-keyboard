519546beb213ab82c4a0bf65742dce3f3720bb43
Pavel Shnurov <pavel.shnurov@gmail.com>
2023-04-29 10:42:33 +0200

    feat: add keyboard

---
+++ virtual-keyboard/src/Keyboard/index.js
@@ -0,0 +1,6 @@
+import htmlToElement from "../utils/htmlToElement";
+import Keyboard from './index.html'
+import './index.scss'
+
+const keyboard = htmlToElement(Keyboard);
+export default keyboard
--- virtual-keyboard/src/index.js
+++ virtual-keyboard/src/index.js
@@ -1,9 +1,21 @@
 import TextArea from './Text-area'
 import Keyboard from './Keyboard'
 import './index.scss'
+import {init} from './Button/index.js'
+
+let keyEng = ['§', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'];
+let keyRus = [];
 
 document.body.innerHTML = `<main></main>`;
 const root = document.querySelector('main')
 root.append(TextArea);
 root.append(Keyboard);
+init(keyEng);
 
+document.onkeydown = function (event) {
+  document.querySelector('.key[data="' + event.key.toLowerCase() + '"]').classList.add('active');
+}
+
+document.onkeyup = function (event) {
+  document.querySelector('.key[data="' + event.key.toLowerCase() + '"]').classList.remove('active');
+}
